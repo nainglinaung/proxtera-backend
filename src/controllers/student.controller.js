0c1acef268df8a51654f06b5c25b7101e6d609a5
Naing Lin Aung <nainglinaung@s2t.ai>
2022-09-25 21:12:53 +0630

    refactor

--- src/controllers/student.controller.js
+++ src/controllers/student.controller.js
@@ -1,38 +1,14 @@
 import StudentModel from "../models/student.model";
-import {readFile,utils} from "xlsx";
-
+import {readFile} from "xlsx";
+import {composeData} from "../repositories/student.repo";
 export default {
   importDoc: async (req, res) => {
     let file = req.file;
     let workbook = await readFile(file.path);
-    let sheets = workbook.SheetNames;
-    let cleanCount = 0; 
-    let invalidRecords = []
-    for (let sheet of sheets) {
-      let sheetData = workbook.Sheets[sheet];
-      let output  = utils.sheet_to_json(sheetData);
-      try {
-        
-        let cleanRecords = [];
-        let missingRecords = [];
+    let {invalidRecords,cleanRecords,missingCount,cleanCount} = composeData(workbook)
 
-        for (let record of output) {
-            if (record.Username && record.University && record.Email) {
-              cleanRecords.push(record);
-            } else {
-              invalidRecords.push(record);
-            }
-        }
-       
-        cleanCount += cleanRecords.length;
-        let result = await StudentModel.insertMany(cleanRecords,{ordered:false,populate:"Array"});
-     } catch(error) {
-        console.error(error);
-        return res.status(400).json({status:"Error Happened"})
-      }
-    }
-
-    return res.json({invalidRecords, missingCount:invalidRecords.length, cleanCount});
+    let result = await StudentModel.insertMany(cleanRecords,{ordered:false,populate:"Array"});
+    return res.json({invalidRecords, missingCount, cleanCount});
        
 
   },
---
+++ start.sh
@@ -0,0 +1,1 @@
+docker compose up & npm run dev
